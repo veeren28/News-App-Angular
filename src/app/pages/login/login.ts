a90fdbfa607d52f577d3351fec2912b468129254
veeren28 <veerenveerupinisetty@gmail.com>
2025-06-23 13:49:28 +0530

    login and registration page

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required, Validators.minLength(6)],
+    });
+  }
+}
---
+++ src/app/pages/registration-page/registration-page.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component,OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+export function MissingAt(control:AbstractControl):ValidationErrors | null{
+ let  value =  control.value;
+ if(value && !value.contains('@')){
+  return {MissingAt:true};
+ }
+ return null;
+
+}
+@Component({
+  selector: 'app-registration-page',
+  imports: [ReactiveFormsModule,CommonModule],
+  templateUrl: './registration-page.html',
+  styleUrl: './registration-page.css'
+})
+export class RegistrationPage implements OnInit{
+  registrationForm!:any;
+  constructor(private fb: FormBuilder){}
+  ngOnInit() {
+    this.registrationForm = this.fb.group({
+      name: ['',Validators.required],
+      phno:['',Validators.required,Validators.minLength(10)],
+      email:['',Validators.required,[containsAt]],
+      username:['',Validators.required],
+      password:['',Validators.required]
+
+    })
+    }
+      
+  }
+
+}
